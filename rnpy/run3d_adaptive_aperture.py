b692dd49420ade545bd25d54d2f746027b9b5f29
Alison Kirkby <a.kirkby@gns.cri.nz>
2021-09-02 08:23:27 +1200

    misc changes to run_adaptive

--- rnpy/run3d_adaptive_aperture.py
+++ rnpy/run3d_adaptive_aperture.py
@@ -15,6 +15,7 @@ import argparse
 import time
 
 trace_mem = True
+save_arrays = False
 if trace_mem:
     import tracemalloc
 
@@ -192,14 +193,15 @@ def run_adaptive(repeats, input_parameters, numfs, outfilename, rank):
             else:
                 stat = 'w'
             with open(iruntimefn,stat) as iruntimefile:
-                iruntimefile.write('%1i %1i %1i %.4f %.4f %.4f %.4f %s\n'%(nx,ny,nx,iruntime, fs, peaksetup, peaksolve,input_parameters['solver_type']))
+                iruntimefile.write('%1i %1i %1i %.4f %.4e %.4f %.4f %s\n'%(nx,ny,nx,iruntime, fs, peaksetup, peaksolve,input_parameters['solver_type']))
          
             RockVolI.compute_conductive_fraction()
             cfractions[i] = RockVolI.conductive_fraction
             resbulk[i] = RockVolI.resistivity_bulk[2]
             kbulk[i] = RockVolI.permeability_bulk[2]
             if r == 0:
-                save_arrays(RockVolI,props_to_save,'r%1i'%r)
+                if save_arrays:
+                    save_arrays(RockVolI,props_to_save,'r%1i'%r)
     
         # run infilling runs
         count = len(fault_separations)
@@ -239,7 +241,7 @@ def run_adaptive(repeats, input_parameters, numfs, outfilename, rank):
             iruntime=time.time()-t0
             print('time taken',iruntime)
             with open(iruntimefn,stat) as iruntimefile:
-                iruntimefile.write('%1i %1i %1i %.4f %.4f %.4f %.4f %s\n'%(nx,ny,nx,iruntime,newfs, peaksetup, peaksolve,input_parameters['solver_type']))
+                iruntimefile.write('%1i %1i %1i %.4f %.4e %.4f %.4f %s\n'%(nx,ny,nx,iruntime,newfs, peaksetup, peaksolve,input_parameters['solver_type']))
          
             # insert resistivity bulk, conductive fraction & new fault separation to arrays
             resbulk = np.insert(resbulk,i+1,RockVol.resistivity_bulk[2])
@@ -251,7 +253,8 @@ def run_adaptive(repeats, input_parameters, numfs, outfilename, rank):
             fault_separations = np.insert(fault_separations,i+1,newfs)
             
             if r == 0:
-                save_arrays(RockVol,props_to_save,'r%1i'%r)
+                if save_arrays:
+                    save_arrays(RockVol,props_to_save,'r%1i'%r)
             
             count += 1
             
