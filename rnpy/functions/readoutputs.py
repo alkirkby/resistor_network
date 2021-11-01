f6ccb1683ea31887c340b9cb94928501afa2e7d1
a.kirkby <a.kirkby@gns.cri.nz>
2021-11-02 10:30:35 +1300

    add some more functions for calculating bulk properties from outputs of simulation results

--- rnpy/functions/readoutputs.py
+++ rnpy/functions/readoutputs.py
@@ -36,3 +36,55 @@ def interpolate_to_all_fs(outputs):
             data_dict1[pname][r] = 10**func(data_dict1['fs'])
             
     return data_dict1
+
+def bulk_permeability(permeability,x_cellsize,cellsize_max,permeability_matrix=1e-18):
+    """
+    Correct permeability results to a constant fault size
+
+    Parameters
+    ----------
+    permeability : float or numpy array, shape (n,)
+        permeability values from simulation.
+    x_cellsize : float or numpy array, shape (n,)
+        x cellsize for each permeability (or a single value).
+    cellsize_max : float
+        maximum cell size to correct to.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    return (permeability*x_cellsize + \
+             permeability_matrix*(cellsize_max - x_cellsize))/cellsize_max
+        
+        
+def bulk_resistivity(resistivity,x_cellsize,cellsize_max,
+                     res_matrix=1000):
+    """
+    Correct resistivity results to a constant fault size
+
+    Parameters
+    ----------
+    resistivity : float or numpy array, shape (n,)
+        resistivity values from simulation.
+    x_cellsize : float or numpy array, shape (n,)
+        x cellsize for each permeability (or a single value).
+    cellsize_max : float
+        maximum cell size to correct to.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    return cellsize_max/((cellsize_max - x_cellsize)/res_matrix +\
+                                     x_cellsize/resistivity)
+
+def bulk_cfraction(cfraction,x_cellsize,cellsize_max):
+    return cfraction*x_cellsize/cellsize_max
+
+def hydraulic_aperture(permeability,conductive_fraction,permeability_matrix = 1e-18):
+    return np.sqrt(12*(permeability-(1-conductive_fraction)*permeability_matrix)/conductive_fraction)
