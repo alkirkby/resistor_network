d010f818cbcd7ff1658d45de066f07285ae2d685
a.kirkby <a.kirkby@gns.cri.nz>
2021-11-04 15:34:26 +1300

    add a test for a fault with a constant aperture

--- tests/core/test_resistor_network.py
+++ tests/core/test_resistor_network.py
@@ -29,7 +29,7 @@ class testResistorNetwork2d(TestCase):
                                     'correct_aperture_for_geometry':False})
         self.fault_separations = [-5e-4,0,1e-4,1e-3]
         
-    def test_random_setup(self):
+    def test_random(self):
         
         
         for i, fs in enumerate(fslist):
@@ -43,13 +43,35 @@ class testResistorNetwork2d(TestCase):
             rv.compute_conductive_fraction()
             cf = rv.conductive_fraction
             
-            print(np.log10(rv.permeability_bulk),np.log10(answers_random[i,:3]))
-            
             assert np.all(np.abs(np.log10(rv.permeability_bulk[1:]) - \
                                  np.log10(answers_random[i,:2])) < 1e-6)
             assert np.all(np.abs(np.log10(rv.resistivity_bulk[1:]) - \
                                  np.log10(answers_random[i,2:4])) < 1e-6)
             assert np.abs(rv.conductive_fraction - \
                           answers_random[i,4]) < 1e-6
+                
+    def test_constant_aperture(self):
+        for i, fs in enumerate(fslist[1:]):
+            rv = Rock_volume(ncells=[0,50,50],cellsize=[1.5e-3,0.00025,0.00025],
+                             aperture_type='constant',
+                            
+                             fault_dict={'fault_separation':fs,
+                                         'random_numbers_dir':r'C:\tmp',
+                                         'correct_aperture_for_geometry':False})
+            rv.solve_resistor_network2()
+            rv.compute_conductive_fraction()
+            cf = rv.conductive_fraction
+            
+            kpar = (fs**3/12 + (rv.cellsize[0]-fs)*1e-18)/rv.cellsize[0]
+            rpar = rv.cellsize[0]/(fs/rv.resistivity_fluid + \
+                                   (rv.cellsize[0]-fs)/rv.resistivity_matrix)
+            
+            print(kpar,rv.permeability_bulk[1:])
+            assert np.all(np.abs(np.log10(rv.permeability_bulk[1:]) - \
+                                 np.log10(kpar)) < 1e-6)
+            assert np.all(np.abs(np.log10(rv.resistivity_bulk[1:]) - \
+                                 np.log10(rpar)) < 1e-6)
+            assert np.abs(rv.conductive_fraction - fs/rv.cellsize[0]) < 1e-6        
+        
 # print(kpar/rv.permeability_bulk)
 
