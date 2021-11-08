fa5f88c2355fb0fe11b7a083c7aeca693c8c9075
a.kirkby <a.kirkby@gns.cri.nz>
2021-11-09 11:33:01 +1300

    add function to interpolate results to permeability values

--- rnpy/functions/readoutputs.py
+++ rnpy/functions/readoutputs.py
@@ -8,6 +8,18 @@ import numpy as np
 from scipy.interpolate import interp1d
 from scipy.stats import sem
 
+
+def update_idx_dict(idx_dict):
+    if idx_dict is None:
+        idx_dict = {'fs':0,'cf':1,'res':2,'k':3,'xcs':5}
+    else:
+        idx_dict2 = {'fs':0,'cf':1,'res':2,'k':3,'xcs':5}
+        idx_dict2.update(idx_dict)
+        idx_dict = idx_dict2
+        
+    return idx_dict
+    
+
 def interpolate_to_all_fs(outputs,idx_dict=None):
     """
     Interpolate outputs from simulations to all fault separations
@@ -27,17 +39,65 @@ def interpolate_to_all_fs(outputs,idx_dict=None):
     data_dict1['fs'] = np.unique(outputs[:,:,0])
     nrpts = outputs.shape[0]
     
-    if idx_dict is None:
-        idx_dict = {'cf':1,'res':2,'k':3,'xcs':5}
+    idx_dict = update_idx_dict(idx_dict)
 
     for pname in idx_dict.keys():
-        i = idx_dict[pname]
-        data_dict1[pname] = np.zeros((nrpts,data_dict1['fs'].shape[0]))
-        for r in range(nrpts):
-            func = interp1d(outputs[r,:,0],np.log10(outputs[r,:,i]),bounds_error=False)
-            data_dict1[pname][r] = 10**func(data_dict1['fs'])
+        if pname != 'fs':
+            i = idx_dict[pname]
+            data_dict1[pname] = np.zeros((nrpts,data_dict1['fs'].shape[0]))
+            for r in range(nrpts):
+                if pname in ['res','k']:
+                    # interpolate in log space
+                    func = interp1d(outputs[r,:,idx_dict['fs']],np.log10(outputs[r,:,i]),bounds_error=False)
+                    data_dict1[pname][r] = 10**func(data_dict1['fs'])
+                else:
+                    # interpolate in linear space
+                    func = interp1d(outputs[r,:,idx_dict['fs']],outputs[r,:,i],bounds_error=False)
+                    data_dict1[pname][r] = func(data_dict1['fs'])   
             
     return data_dict1
+
+
+def interpolate_to_permeability_values(outputs,permeability_values,idx_dict=None):
+    """
+    Interpolate outputs from simulations to all fault separations
+
+    Parameters
+    ----------
+    outputs : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    data_dict1 : TYPE
+        DESCRIPTION.
+
+    """
+    data_dict1 = {}
+    data_dict1['k'] = permeability_values
+    nrpts = outputs.shape[0]
+    
+    idx_dict = update_idx_dict(idx_dict)
+
+
+    for pname in idx_dict.keys():
+        if pname != 'k':
+            i = idx_dict[pname]
+            data_dict1[pname] = np.zeros((nrpts,data_dict1['k'].shape[0]))
+            for r in range(nrpts):
+                if pname in ['res','cf']:
+                    # interpolate in log space
+                    func = interp1d(np.log10(outputs[r,:,idx_dict['k']]),np.log10(outputs[r,:,i]),bounds_error=False)
+                    data_dict1[pname][r] = 10**func(np.log10(data_dict1['k']))
+                else:
+                    # interpolate in linear space (but log permeability)
+                    # func = interp1d(outputs[r,:,idx_dict['k']],outputs[r,:,i],bounds_error=False)
+                    # data_dict1[pname][r] = func(data_dict1['k'])     
+                    func = interp1d(np.log10(outputs[r,:,idx_dict['k']]),outputs[r,:,i],bounds_error=False)
+                    data_dict1[pname][r] = func(np.log10(data_dict1['k']))              
+            
+    return data_dict1
+
 
 def bulk_permeability(permeability,x_cellsize,cellsize_max,permeability_matrix=1e-18):
     """
@@ -117,4 +177,4 @@ def getmean(vals,mtype='meanlog10',semm=0):
     if mtype=='meanlog10':
         return 10**(np.mean(np.log10(vals),axis=0)+semm*sem(np.log10(vals),axis=0))
     elif mtype == 'median':
-        return np.median(np.log10(vals)) + semm*sem(np.log10(vals),axis=0)
+        return 10**(np.median(np.log10(vals),axis=0) + semm*sem(np.log10(vals),axis=0))
