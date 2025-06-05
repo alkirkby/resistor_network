update calculation of equivalent resistivity, and try to fix tests

rnpy/functions/readoutputs.py
@@ -473,6 +473,6 @@ def get_equivalent_rho(rho,width,equivalent_width, rho_matrix=1000):
     None.
 
     '''
-    pad = np.maximum(equivalent_width - width, 0.)
+    pad = equivalent_width - width
     
     return equivalent_width/(pad/rho_matrix + width/rho)

tests/core/test_resistor_network.py
@@ -92,8 +92,8 @@ class testResistorNetwork2d(TestCase):
             rv.compute_conductive_fraction()
             
             kpar = (fs**3/12 + (rv.cellsize[0]-fs)*1e-18)/rv.cellsize[0]
-            rpar = rv.cellsize[0]/(fs/rv.resistivity_fluid + \
-                                   (rv.cellsize[0]-fs)/rv.resistivity_matrix)
+            rpar = rv.cellsize[1:]/(fs/rv.resistivity_fluid + \
+                                   (rv.cellsize[1:]-fs)/rv.resistivity_matrix[1:])
             
             assert np.all(np.abs(np.log10(rv.permeability_bulk[1:]) - \
                                  np.log10(kpar)) < 1e-6)