Fix alpha calculation in compute_2d_sticks_dask.py

rnpy/compute_2d_sticks_dask.py
@@ -108,11 +108,13 @@ def setup_and_solve_fault_sticks(Nf, fault_lengths_m, fault_widths, hydraulic_ap
     t0 = time.time()
     # assign faults to array:
     count1 = 0
+    fault_lengths_assigned = None
     while np.any(Nf_update) > 0:
         for ii,Nfval in enumerate(Nf_update):
-            Rv = add_random_fault_sticks_to_arrays(Rv, Nfval, fault_lengths_m[ii], fault_widths[ii], hydraulic_apertures[ii],
-                                           resistivity[ii],pz)
-        Nf_update_new = ((np.array(Nf) - np.array([np.sum(Rv.aperture[:,:,1,1:,1:]==fault_widths[i])/(fault_lengths_m[i]/Rv.cellsize[1]) for i in range(len(Nf))]))).astype(int)
+            Rv, fault_lengths_assigned = add_random_fault_sticks_to_arrays(Rv, Nfval, fault_lengths_m[ii], fault_widths[ii], hydraulic_apertures[ii],
+                                           resistivity[ii],pz,fault_lengths_assigned=fault_lengths_assigned)
+        Nf_update_new = ((np.array(Nf) - np.array([np.sum(fault_lengths_assigned==fault_lengths_m[i])/(fault_lengths_m[i]/Rv.cellsize[1]) for i in range(len(Nf))]))).astype(int)
+        np.save(r'C:\tmp\fault_lengths_assigned',fault_lengths_assigned)
         if np.all(np.array(Nf_update_new) == np.array(Nf_update)):
             count += 1
         else:
@@ -207,6 +209,7 @@ if __name__ == '__main__':
                                                   'fault_k_aperture_rf%s_%s.npy'%(rfluid,direction)),
                                                            None)
     
+    
     if len(np.unique(lvals_center)) < len(lvals_center):
             
         lvals_center_unique = np.unique(lvals_center)
@@ -215,8 +218,8 @@ if __name__ == '__main__':
     else:
         lvals_center_unique, fw_mean = lvals_center, fw
 
-    R2_for_alpha_calc = (10*lvals_center_unique.max())**2
-    print(R2_for_alpha_calc)
+    R_for_alpha_calc = 10*lvals_center_unique.max()
+
     t1a = time.time()
     print('get lvals, %.2fs'%(t1a-t0))
     
@@ -224,9 +227,9 @@ if __name__ == '__main__':
     print(a,R,lvals_center_unique,fw_mean,porosity_target)
     
     # use big R-squared (R2) to compute alpha
-    alpha, lvals_range = get_alpha(a,R2_for_alpha_calc,lvals_center_unique,fw_mean,porosity_target,alpha_start = 0.1)
-    print("alpha",alpha)
-    Nf = get_Nf2D(a, alpha, R**2, lvals_range)
+    alpha, lvals_range = get_alpha(a,R_for_alpha_calc,lvals_center_unique,fw_mean,porosity_target,alpha_start = 0.1)
+
+    Nf = get_Nf2D(a, alpha, R, lvals_range)
 
     
     

rnpy/functions/assignfaults_new.py
@@ -34,7 +34,7 @@ def get_faultlength_distribution(lvals,volume,alpha=10,a=3.5):
     
     return Nf
     
-def get_Nf2D(a, alpha, R2, lvals_range):
+def get_Nf2D(a, alpha, R, lvals_range):
     '''
     Get Number of faults within area R within bin ranges provided by lvals_range
 
@@ -54,11 +54,11 @@ def get_Nf2D(a, alpha, R2, lvals_range):
     Number of faults in each bin range (len(lvals_range)-1).
 
     '''
-    
     Nf = []
     for i in range(len(lvals_range)-1):
         lmin,lmax = lvals_range[i:i+2]
-        Nf = np.append(Nf, int(round(alpha/(a-1.)*lmin**(1.-a)*R2 - alpha/(a-1.)*lmax**(1.-a)*R2))).astype(int)
+        Nf = np.append(Nf, round(alpha/(a-1.)*lmin**(1.-a)*R**2 - alpha/(a-1.)*lmax**(1.-a)*R**2)).astype(int)
+        # Nf = np.append(Nf, alpha/(a-1.)*lmin**(1.-a)*R2 - alpha/(a-1.)*lmax**(1.-a)*R2)
 
     return Nf
 
@@ -834,11 +834,16 @@ def update_from_precalculated(rv,effective_apertures_fn,permeability_matrix=1e-1
 
 
 def add_random_fault_sticks_to_arrays(Rv, Nfval, fault_length_m, fault_width, 
-                                      hydraulic_aperture, resistivity,pz):
+                                      hydraulic_aperture, resistivity,pz,
+                                      fault_lengths_assigned=None):
 
     ncells = Rv.ncells[1]
     cellsize = Rv.cellsize[1]
     Rv.aperture_electric[np.isfinite(Rv.aperture_electric)] = cellsize
+    
+    # array to record what fault lengths were assigned where
+    if fault_lengths_assigned is None:
+        fault_lengths_assigned = np.zeros_like(Rv.resistivity)
     
 
     if Nfval > 0:
@@ -967,10 +972,11 @@ def add_random_fault_sticks_to_arrays(Rv, Nfval, fault_length_m, fault_width,
                 Rv.aperture_hydraulic[idx_jj, idx_ii,1,idxc_i,idxo_i] = values_to_assign['aph']
                 Rv.resistance[idx_jj, idx_ii,1,idxc_i] = values_to_assign['resf']/cellsize
                 Rv.resistivity[idx_jj, idx_ii,1,idxc_i] = values_to_assign['resf']
+                fault_lengths_assigned[idx_jj, idx_ii, 1, idxc_i] = fault_length_m
                 idx_i[np.all([faultsi>0],axis=0)] += 1
                 idx_j[np.all([faultsj>0],axis=0)] += 1
                 
                 idx_i[idx_i > ncells + 1] = ncells + 1
                 idx_j[idx_j > ncells + 1] = ncells + 1
 
-    return Rv
+    return Rv, fault_lengths_assigned

rnpy/functions/readoutputs.py
@@ -390,7 +390,7 @@ def read_header(fn):
                     return header_names
 
 
-def load_outputs(fn,clip=0):
+def load_outputs(fn,clip=0,**genfromtxt_args):
     """
     
 
@@ -405,7 +405,7 @@ def load_outputs(fn,clip=0):
 
     """
     
-    outputs = np.genfromtxt(fn,names=read_header(fn))
+    outputs = np.genfromtxt(fn,names=read_header(fn),**genfromtxt_args)
     
     nr = len(np.unique(outputs['repeat']))
     nfs = int(len(outputs)/nr)